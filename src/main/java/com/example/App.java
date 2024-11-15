0f2e65b240d3af7fd65c6119ead31026cda7c952
Pablo Elgueta <pablo.elgueta@externos.hdi.cl>
2024-11-15 00:46:20 -0300

    fix

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,25 +1,20 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-        // Mantener la aplicación corriendo indefinidamente
-        try {
-            Thread.sleep(Long.MAX_VALUE);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+@SpringBootApplication
+@RestController
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 
-    public int add(int a, int b)
-    {
-        return a + b;
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
     }
 }
