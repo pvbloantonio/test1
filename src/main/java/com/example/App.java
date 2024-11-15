cb8f3db1365b13230e3a118b6741a302e1603a23
Pablo Elgueta <pablo.elgueta@externos.hdi.cl>
2024-11-15 00:35:31 -0300

    fix

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,11 +9,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        // Mantener la aplicación corriendo indefinidamente
+        try {
+            Thread.sleep(Long.MAX_VALUE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public int add(int a, int b)
     {
-        return a +b;
+        return a + b;
     }
-    
 }
