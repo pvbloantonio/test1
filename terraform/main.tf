6839da8652761120b86dc23a59ceeea1274f6219
Pablo Elgueta <pablo.elgueta@externos.hdi.cl>
2024-11-16 02:36:50 -0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,21 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.21.0"
+    }
+  }
+}
+
 provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
-resource "docker_image" "nginx" {
-  name = "nginx:latest"
-}
-
-resource "docker_container" "nginx" {
+resource "docker_container" "nginx_container" {
   image = docker_image.nginx.latest
   name  = "nginx_container"
-  ports {
-    internal = 80
-    external = 8081
-  }
+}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
 }
